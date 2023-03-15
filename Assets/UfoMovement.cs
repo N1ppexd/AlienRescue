407bef074c85ae6886ad6c309cf952ddec6c2c3a
N1ppexd <niilohanni@gmail.com>
2023-03-15 13:20:20 +0200

    liikkumista ja kameraa tehty

--- Assets/UfoMovement.cs
+++ Assets/UfoMovement.cs
@@ -1,22 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UfoMovement : MonoBehaviour
 {
+    public InputMaster inputMaster;
 
+
+    [SerializeField] private Rigidbody rb;  //ufon rigidbody...
+
+
+    private Vector2 inputAxis;
+    private Vector3 movementAxis;
     [SerializeField] private float speed;//ufon nopeus...
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        inputMaster = new InputMaster();
+        inputMaster.Enable();
+
+        inputMaster.Player.Move.performed += ctx => inputAxis = ctx.ReadValue<Vector2>();
+        inputMaster.Player.Move.canceled += ctx => inputAxis = ctx.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //liikkumishomma alhaalla
+    #region movement
+    private void Movement(Vector2 axis)
     {
+
+        Debug.Log(axis + " on axis");
+        //koska y on ylöäspäin, tehdään näin. Pelissä ufo ei pyöäti ympäriinsä niin tämä on ok
+        movementAxis = new Vector3(axis.x, 0, axis.y);
+
+        rb.velocity = movementAxis.normalized * speed; //lisätään noppeus...
+
+    }
+    #endregion
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Movement(inputAxis);
         
     }
 }
