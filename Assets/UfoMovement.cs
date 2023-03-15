901e66bb25fad15cb30dbe8d8827d229b98a553d
N1ppexd <niilohanni@gmail.com>
2023-03-15 12:10:16 +0200

    hommelijuttu

--- Assets/UfoMovement.cs
+++ Assets/UfoMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class UfoMovement : MonoBehaviour
 {
+
+    [SerializeField] private float speed;//ufon nopeus...
+
+
     // Start is called before the first frame update
     void Start()
     {
