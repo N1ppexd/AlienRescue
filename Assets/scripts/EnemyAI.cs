7dae27f64a57df00a8c170b7569a6a6d237109a1
N1ppexd <niilohanni@gmail.com>
2023-03-16 12:39:27 +0200

    vihu ai:n alkukantaisen jutun aloitus

--- Assets/scripts/EnemyAI.cs
+++ Assets/scripts/EnemyAI.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private float wanderRadius;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
