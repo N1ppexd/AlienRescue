d5727dadafda2371bf3940b11fac03d890880c8b
N1ppexd <niilohanni@gmail.com>
2023-03-24 10:13:07 +0200

    RESOLUUTOTAT VOI VAIHRTAA

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
 
-    private string screenModeString = "screenMode"; // k‰ytet‰‰n playerPrefiss‰...
+    private string screenModeString = "screenMode", screenResolutionString = "res"; // k‰ytet‰‰n playerPrefiss‰...
 
     [SerializeField] GameObject settingsPanel; //laitetaan pois p‰‰lt‰, kun poistutaan...
+
+    [SerializeField] Dropdown resolution, windowMode;
 
     // Start is called before the first frame update
     void Start()
     {
         if (PlayerPrefs.HasKey(screenModeString))
         {
-            ApplyScreenMode(PlayerPrefs.GetInt(screenModeString));
+            int mode = PlayerPrefs.GetInt(screenModeString);
+            ApplyScreenMode(mode);
+            windowMode.SetValueWithoutNotify(mode);
+        }
+        if (PlayerPrefs.HasKey(screenResolutionString))
+        {
+            int mode = PlayerPrefs.GetInt(screenResolutionString);
+            ApplyResolution(mode);
+            resolution.SetValueWithoutNotify(mode);
         }
     } 
 
@@ -23,6 +34,11 @@ public class SettingsMenu : MonoBehaviour
     {
         ApplyScreenMode(mode);
         PlayerPrefs.SetInt(screenModeString, mode);
+    }
+    public void ChangeResolution(int mode)
+    {
+        ApplyResolution(mode);
+        PlayerPrefs.SetInt(screenResolutionString, mode);
     }
 
 
@@ -41,6 +57,27 @@ public class SettingsMenu : MonoBehaviour
             case 2:
                 //fulscreen windowed
                 Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                return;
+        }
+
+        Debug.LogError("screenModea ei ole... ei vaihdeta..");
+    }
+
+    private void ApplyResolution(int mode)//t‰‰ll‰ oikeasti applyataan screenMode....
+    {
+        switch (mode)
+        {
+            case 0:
+                //1920 x 1080
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                return;
+            case 1:
+                //1280 x 720
+                Screen.SetResolution(1280, 720, Screen.fullScreen);
+                return;
+            case 2:
+                //2560 x 1440
+                Screen.SetResolution(2560, 1440, Screen.fullScreen);
                 return;
         }
 
