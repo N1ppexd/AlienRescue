0fa143d72944d95f087ac385feb8993db0623b67
N1ppexd <niilohanni@gmail.com>
2023-04-05 11:00:06 +0300

    alkutekstit

--- Assets/StoryText.cs
+++ Assets/StoryText.cs
@@ -5,6 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class StoryText : MonoBehaviour
 {
+
+    InputMaster inputMaster;
+    private void Awake()
+    {
+        inputMaster = new InputMaster();
+
+        inputMaster.Player.fastMove.performed += _ => EndTexts();
+    }
+
+    private void OnEnable()
+    {
+        inputMaster.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputMaster.Disable();
+    }
     public void EndTexts()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
