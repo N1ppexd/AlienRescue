e7fd206f0c37d003d27351b58a3b5a4da90034d9
N1ppexd <niilohanni@gmail.com>
2023-04-05 10:49:04 +0300

    juttuja

--- Assets/StoryText.cs
+++ Assets/StoryText.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StoryText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void EndTexts()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
